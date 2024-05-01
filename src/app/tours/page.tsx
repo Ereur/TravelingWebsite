39825b146c9cd6f0b5cd5e744c938d8c06b503de
aamoussa <amoussaouianas@gmail.com>
2024-05-01 05:54:24 +0000

    Search Cards styling and scrooling behavior

--- src/app/tours/page.tsx
+++ src/app/tours/page.tsx
@@ -1,7 +1,9 @@
+"use client";
 import { Input } from "@/components/ui/input";
 import { DateRangePicker } from "@/components/DateRangePicker";
 import { DropdownMenuCheckboxes } from "@/components/DropdownMenuCheckbox";
 import { PersonesMenu } from "@/components/PersonesMenu";
+import { TourCard } from "@/components/TourCard";
 
 export default function tours() {
   return (
@@ -24,7 +26,7 @@ export default function tours() {
       </div>
       <div
         id="Search"
-        className="relative flex z-20  flex-col justify-center w-full h-[500px] items-center pt-32 gap-8"
+        className="relative flex z-20  flex-col justify-center w-full h-[250px] items-center pt-32 gap-8"
       >
         <div className="flex flex-col gap-4 items-center">
           <h1 className="text-[#D9B99B] xl:text-[2rem] text-2xl tracking-widest font-montserrat font-bold">
@@ -34,7 +36,9 @@ export default function tours() {
             Where History Meets Adventure
           </h1>
         </div>
-        <div className="relative grid grid-cols-3  w-[750px] z-20 h-[65px]  rounded-3xl  border-4 border-[#B2AC88] overflow-hidden">
+      </div>
+      <div className="sticky top-[20px] flex z-20 justify-center items-center">
+        <div className="mt-20 grid grid-cols-3 bg-background w-[750px] z-20 h-[65px]  rounded-3xl  border-4 border-[#B2AC88] overflow-hidden">
           <PersonesMenu />
           <DateRangePicker
             className=" w-full"
@@ -46,6 +50,27 @@ export default function tours() {
           /> */}
           {/* <DateRangePicker className=" w-full" ButtonClassName="w-full justify-start h-full  text-left font-normal  border-[#5B340A] hover:bg-[#916A2D] " /> */}
           <DropdownMenuCheckboxes />
+        </div>
+      </div>
+      <div className="h-[20px]"></div>
+      <div className="flex justify-between items-center px-9 flex-wrap">
+        <div className="w-1/3">
+          <TourCard />
+        </div>
+        <div className="w-1/3">
+          <TourCard />
+        </div>
+        <div className="w-1/3">
+          <TourCard />
+        </div>
+        <div className="w-1/3">
+          <TourCard />
+        </div>
+        <div className="w-1/3">
+          <TourCard />
+        </div>
+        <div className="w-1/3">
+          <TourCard />
         </div>
       </div>
     </>
